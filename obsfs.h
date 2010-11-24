94839ce71fe9cefa8f1948c993f0e995cd9c38a2
Ulrich Hecht <uli@suse.de>
2010-11-24 11:51:10 +0100

    licensing information

--- obsfs.h
+++ obsfs.h
@@ -1,3 +1,23 @@
+/*
+ * obsfs.h
+ * (c) 2010 Ulrich Hecht, SuSE Linux Products GmbH <uli@suse.de>
+ *
+ * This file is part of obsfs.
+ *
+ * obsfs is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 or version 3 of the License.
+ *
+ * obsfs is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with obsfs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 /* cache timeouts */
 
 /* Directories are updated frequently as packages build or fail, but
